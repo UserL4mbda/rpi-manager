20af08fcf625bb17d57bc623a6349b16e1071726
UserL4mbda <stephane.dany@gmail.com>
2024-12-08 17:56:58 +0100

    Ajout de l'ajout du bridge

--- main.go
+++ main.go
@@ -37,5 +37,34 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"output": string(output)})
 	})
 
+	r.POST("/bridge", func(c *gin.Context) {
+		cmds := []*exec.Cmd{
+			exec.Command("ip", "link", "add", "name", "br0", "type", "bridge"),
+
+			exec.Command("ip", "link", "set", "eth0", "master", "br0"),
+			exec.Command("ip", "link", "set", "eth1", "master", "br0"),
+
+			exec.Command("ip", "link", "set", "br0", "up"),
+			exec.Command("ip", "link", "set", "eth0", "up"),
+			exec.Command("ip", "link", "set", "eth1", "up"),
+
+			exec.Command("ip", "link", "set", "eth0", "promisc", "on"),
+			exec.Command("ip", "link", "set", "eth1", "promisc", "on"),
+
+			exec.Command("ip", "addr", "flush", "dev", "eth0"),
+			exec.Command("ip", "addr", "flush", "dev", "eth1"),
+		}
+
+		for _, cmd := range cmds {
+			err := cmd.Run()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"message": "Bridge created"})
+		}
+
+	})
+
 	r.Run(":8080")
 }
